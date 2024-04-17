dd92bc2354f4c50c36cf1fa350eab95df8c19c09
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-04-16 18:09:14 -0600

    deleted files for do over

--- src/controller/usersRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createUser } from "../service/userService.js";
-
-const userRouter = Router();
-userRouter.post('/', createUser);
-
-export default userRouter;
--- src/model/associations.js
+++
@@ -1,6 +0,0 @@
-// A.belongsTo(B) association means One-To-One
-// A.hasOne(B) association means One-To-One
-// A.hasMany(B) association means One-To-Many
-// A.belongsToMany(B, { through: 'C' }) association means Many-To-Many relationship, using table C as junction table
-
-import { User } from "./user.js";
