88b9dee8cd8bac193923bc835b68b75d87a1700c
Juan_Bedoya_Barbosa <102477075+JBedoya-Barbosa@users.noreply.github.com>
2024-04-19 16:44:16 -0600

    setup .env.development

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,36 @@
+import path from "path";
+import dotenv from "dotenv";
+
+// Initializing environment variables
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+console.log(`Running in mode: ${mode}`);
+dotenv.config({
+  path:path.join(__dirname, `../.env.${mode}`)
+});
+
 import express from "express";
 import cors from "cors";
 import router from "./routes/router";
 import { sequelize } from "./config/db";
 
-
 const app = express();
 const port = 5555;
 
-// MIDDLEWARES
-
 app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
 app.use(router);
 
 async function main() {
-
-    await sequelize.sync({ force: false });
-    console.log("Connected to DB");
-    app.listen(port, () => {
-        console.log(`Server is running on port: ${port}. Visit http://localhost:${port} to check the API!`);
-        });
+  await sequelize.sync({ force: false });
+  console.log("Connected to DB");
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}. Visit http://localhost:${port} to check the API!`);
+  });
 }
 
 main();
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,13 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
+const dbName : string = process.env.DB_NAME || "";
+const dbUsername : string = process.env.DB_USERNAME || "";
+const dbPassword : string = process.env.DB_PASSWORD || "";
+const dbHost : string = process.env.DB_HOST || "";
+const dbPort : number = parseInt(process.env.DB_PORT || "3306");
 
-const name: string = process.env.DB_NAME_WRITE || "";
-const user: string = process.env.DB_USER_WRITE || "";
-const password: string = process.env.DB_PASSWORD_WRITE || "";
-const host: string = process.env.DB_HOST_WRITE || "";
-const port: number = parseInt(process.env.DB_PORT_WRITE || "3306");
-
-export const sequelize = new Sequelize(name, user, password, {
-  host: host,
+export const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
+  host: dbHost,
   dialect: "mysql",
-  port: port,
+  port: dbPort,
 });
