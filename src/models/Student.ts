8416557c7b75b82e1361aec9be28283c99d9532c
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-04-20 20:18:51 -0600

    Created Student

---
+++ src/models/Student.ts
@@ -0,0 +1,57 @@
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+import { sequelize } from "../config/db";
+import User from "./User";
+
+class Student extends Model<
+  InferAttributes<Student>,
+  InferCreationAttributes<Student>
+> {}
+
+Student.init(
+  {
+    user: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    school: {
+      type: DataTypes.UUID,
+    },
+    fieldPosition: {
+      type: DataTypes.UUID,
+    },
+    shirtNumber: {
+      type: DataTypes.INTEGER,
+    },
+    team: {
+      type: DataTypes.UUID,
+    },
+    CURP: {
+      type: DataTypes.STRING,
+    },
+    IMSS: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    tableName: "Student",
+    modelName: "Student",
+  }
+);
+
+Student.belongsTo(User, {
+  foreignKey: "user",
+});
+
+User.hasOne(Student)
+
+export default Student;
