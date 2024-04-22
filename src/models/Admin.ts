d757c948afe2e2d168eedf033ac9ae8412f878c3
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-04-22 14:47:50 -0600

    Added types to admin

--- src/models/Admin.ts
+++ src/models/Admin.ts
@@ -1,8 +1,17 @@
-import { DataTypes, Model } from "sequelize";
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model, NonAttribute } from "sequelize";
 import { sequelize } from "../config/db";
 import User from "./User";
 
-class Admin extends Model {};
+class Admin extends Model<InferAttributes<Admin>, InferCreationAttributes<Admin>> {
+    declare id: string;
+    declare ine: string;
+    declare user: NonAttribute<User>;
+
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+    declare deletedAt: CreationOptional<Date | null>;
+
+};
 
 Admin.init(
     {
@@ -18,12 +27,25 @@ Admin.init(
         ine: {
             type: DataTypes.STRING,
             allowNull: false,
-        }
+        },
+
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
+        
+        },
+        deletedAt: {
+            type: DataTypes.DATE
+        },
     },
     {
         sequelize,
+        timestamps: true, 
         tableName: "Admin",
         modelName: "Admin",
+        paranoid: true,
     }
 )
 
