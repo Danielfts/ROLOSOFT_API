740cf36df710b937cbede8acea18d2ab085c3551
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-04-24 15:15:34 -0600

    Refactoring

--- src/services/GenderService.ts
+++ src/services/GenderService.ts
@@ -2,11 +2,11 @@ import Gender from "../models/Gender";
 
 class GenderService {
     public static async setupGenders(): Promise<void> {
-        
-        Gender.findOrCreate({where: { name: "MALE" }});
-        Gender.findOrCreate({where: { name: "FEMALE"}});
-        Gender.findOrCreate({where: { name: "OTHER"}});
+        const genders = ["MALE", "FEMALE", "OTHER"];
 
+        await Promise.all(genders.map(async (name) => {
+            await Gender.findOrCreate({ where: { name } });
+        }));
     }
 }
 
