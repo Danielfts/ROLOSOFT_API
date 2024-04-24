e273c4aa34f6655eeebcff4bdaf244db8480174c
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-04-24 15:14:38 -0600

    Added default dev admin

--- src/utils/dbSetup.ts
+++ src/utils/dbSetup.ts
@@ -2,10 +2,27 @@ import Address from "../models/Address";
 import Admin from "../models/Admin";
 import Gender from "../models/Gender";
 import Parent from "../models/Parent";
+import Roles from "../models/Roles";
 import Student from "../models/Student";
 import User from "../models/User";
 import GenderService from "../services/GenderService";
+import UserService from "../services/UserService";
 
 export async function setupDatabase() {
-    await GenderService.setupGenders();
+  const mode = process.env.NODE_ENV;
+  await GenderService.setupGenders();
+
+  if (mode === "development") {
+    await UserService.createUser({
+      firstName: "Admin",
+      lastName: "",
+      email: "admin@hotmail.com",
+      password: "admin",
+      birthDate: new Date(),
+      gender: "MALE",
+      phone: "0000000000",
+      role: Roles.admin,
+      admin: { INE: "0000000000" },
+    });
+  }
 }
