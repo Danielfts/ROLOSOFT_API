32e2b4537615f9f820a13d4b91ba32d56cbf8b8c
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-05-03 01:31:17 -0600

    Completed tournament create and get operations with addresses

--- src/models/Tournament.ts
+++ src/models/Tournament.ts
@@ -1,43 +1,74 @@
 import { UUID } from "crypto";
-import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import {
+  BelongsToGetAssociationMixin,
+  CreationOptional,
+  DataTypes,
+  ForeignKey,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  NonAttribute,
+} from "sequelize";
 import { sequelize } from "../config/db";
+import Address from "./Address";
 
-class Tournament extends Model<InferAttributes<Tournament>, InferCreationAttributes<Tournament>>{
-    declare id: CreationOptional<UUID>;
-    declare name: string;
-    declare startDate: Date;
-    declare endDate: Date;
-    declare address: string;
+class Tournament extends Model<
+  InferAttributes<Tournament>,
+  InferCreationAttributes<Tournament>
+> {
+  declare id: CreationOptional<UUID>;
+  declare name: string;
+  declare startDate: Date;
+  declare endDate: Date;
+  declare Address: NonAttribute<Address>;
+  declare addressId: ForeignKey<UUID>;
+
+  declare getAddress: BelongsToGetAssociationMixin<Address>;
 }
 
-Tournament.init({
+Tournament.init(
+  {
     id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     startDate: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
+      type: DataTypes.DATEONLY,
+      allowNull: false,
     },
     endDate: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
+      type: DataTypes.DATEONLY,
+      allowNull: false,
     },
-    address: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-}, {
+    addressId: {
+      type: DataTypes.UUID,
+      references: {
+        model: "Address",
+        key: "id",
+      },
+    },
+  },
+  {
     sequelize,
     tableName: "Tournaments",
     modelName: "Tournament",
     timestamps: true,
-    paranoid: true
-})
+    paranoid: true,
+  }
+);
+
+Tournament.belongsTo(Address, {
+  foreignKey: "addressId",
+});
+
+Address.hasOne(Tournament, {
+  foreignKey: "addressId",
+});
 
 export default Tournament;
