b5f06379d6f2b5d0eabb3c624c2d63095cb7bbac
Danielfts <d.trivino@uniandes.edu.co>
2024-05-23 19:20:23 -0600

    added dummy get matches by tournament endpoint

--- src/routes/tournamentRouter.ts
+++ src/routes/tournamentRouter.ts
@@ -7,6 +7,9 @@ import MatchController from "../controllers/MatchController";
 import SchoolController from "../controllers/SchoolController";
 import UserController from "../controllers/UserController";
 import Match from "../models/Match";
+import { UUIDV4 } from "sequelize";
+import { StatusCodes } from "http-status-codes";
+import JSONResponse from "../dtos/JSONResponse";
 
 const tournamentRouter = Router();
 
@@ -30,14 +33,14 @@ tournamentRouter.post(
   validateClient,
   // TeamController.createTeam
   SchoolController.registerSchoolInTournament
-)
+);
 
 // Get all schools in a tournament
 tournamentRouter.get(
   "/:tournamentId/schools",
   validateClient,
   SchoolController.getSchoolsByTournament
-)
+);
 
 //TOURNAMENT STUDENTS
 
@@ -45,7 +48,7 @@ tournamentRouter.get(
   "/:tournamentId/players",
   validateClient,
   UserController.getStudentsByTournament
-)
+);
 
 //PHASES
 // GET AVAILABLE PHASES
@@ -53,7 +56,7 @@ tournamentRouter.get(
   "/:tournamentId/phases",
   validateClient,
   PhaseController.getAllPhasesByTournament
-)
+);
 
 //MATCHES
 // CREATE
@@ -61,14 +64,112 @@ tournamentRouter.post(
   "/:tournamentId/phases/:phaseId/matches",
   validateClient,
   MatchController.createMatch
-)
+);
 
 // GET ALL
 tournamentRouter.get(
-  "/:tournamentId",
+  "/:tournamentId/matches",
   validateClient,
-  MatchController.getAllMatches
-)
-
+  (req, res, next) => {
+    const dummy = [
+      {
+        dateStart: new Date(),
+        dateEnd: new Date(),
+        isPlaying: true,
+        teamA: {
+          id: 1,
+          name: "santa fe",
+          points: 20,
+          shieldImg: "null",
+          goals: [
+            {
+              name: "pepito",
+              lastName: "gomelin",
+              minute: 90,
+              player_number: 10,
+            },
+            {
+              name: "juanito",
+              lastName: "ñero",
+              minute: 80,
+              player_number: 11,
+            },
+          ],
+        },
+        teamB: {
+          id: 2,
+          points: 18,
+          name: "junior",
+          shieldImg: "null",
+          goals: [
+            {
+              name: "pepito",
+              lastName: "caremonda",
+              minute: 90,
+              player_number: 10,
+            },
+            {
+              name: "juanito",
+              lastName: "caremonda",
+              minute: 80,
+              player_number: 11,
+            },
+          ],
+        },
+      },
+      {
+        dateStart: new Date(),
+        dateEnd: new Date(),
+        isPlaying: true,
+        teamA: {
+          id: 3,
+          name: "santa fe",
+          points: 20,
+          shieldImg: "null",
+          goals: [
+            {
+              name: "pepito",
+              lastName: "gomelin",
+              minute: 90,
+              player_number: 10,
+            },
+            {
+              name: "juanito",
+              lastName: "ñero",
+              minute: 80,
+              player_number: 11,
+            },
+          ],
+        },
+        teamB: {
+          id: 4,
+          points: 18,
+          name: "junior",
+          shieldImg: "null",
+          goals: [
+            {
+              name: "pepito",
+              lastName: "caremonda",
+              minute: 90,
+              player_number: 10,
+            },
+            {
+              name: "juanito",
+              lastName: "caremonda",
+              minute: 80,
+              player_number: 11,
+            },
+          ],
+        },
+      },
+    ];
+    const response: JSONResponse = {
+      success: true,
+      message: "Matches retrieved successfully",
+      data: dummy,
+    };
+    res.status(StatusCodes.OK).json(response);
+  }
+);
 
 export default tournamentRouter;
