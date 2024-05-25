595e299ddcf61d1ced48e7d52c8092f8b0e64a89
Danielfts <d.trivino@uniandes.edu.co>
2024-05-25 15:55:05 -0600

    Added default data for dev environment

--- src/utils/dbSetup.ts
+++ src/utils/dbSetup.ts
@@ -1,7 +1,8 @@
-
 import Roles from "../models/Roles";
 
 import GenderService from "../services/GenderService";
+import SchoolService from "../services/SchoolService";
+import TournamentService from "../services/TournamentService";
 import UserService from "../services/UserService";
 
 export async function setupDatabase(): Promise<void> {
@@ -26,7 +27,66 @@ export async function setupDatabase(): Promise<void> {
         state: "Estado",
         postalCode: "00000",
         country: "País",
-      }, 
+      },
     });
+
+    const torneo = {
+      name: "COPA COLOMBIA",
+      startDate: new Date(),
+      endDate: new Date(),
+      address: {
+        address1: "Calle 1",
+        address2: "Calle 2",
+        city: "Ciudad",
+        state: "Estado",
+        postalCode: "00000",
+        country: "País",
+      },
+    };
+    const torneoCreado = await TournamentService.createTournament(torneo);
+
+    const estudiante = {
+      CURP: "123123123",
+      firstName: "David",
+      lastName: "Beltran",
+      email: "david@hotmail.com",
+      birthDate: new Date(),
+      gender: "MALE",
+      role: "student",
+      phone: "1231231231",
+      password: "caremonda",
+      student: {
+        fieldPosition: "AGUATERO",
+        shirtNumber: 10,
+        IMSS: "sdñafjk",
+      },
+      address: {
+        address1: "Calle 1",
+        address2: "Calle 2",
+        city: "Ciudad",
+        state: "Estado",
+        postalCode: "00000",
+        country: "País",
+      },
+    };
+    const estudianteCreado = await UserService.createUser(estudiante);
+
+    const escuela = {
+      number: 10,
+      name: "COLEGIO FRESA",
+      address: {
+        address1: "Calle 1",
+        address2: "Calle 2",
+        city: "Ciudad",
+        state: "Estado",
+        postalCode: "00000",
+        country: "País",
+      },
+    };
+    const escuelaCreada = await SchoolService.createSchool(escuela);
+  
+    await SchoolService.registerSchoolInTournament(torneoCreado.id!, escuelaCreada.id!, "CERVEZA AGUILA", [estudianteCreado.id]);
+
   }
+
 }
