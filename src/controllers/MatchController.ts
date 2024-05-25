86202c04d3f62c9f5ddf6592e519a47c91bc9a08
Danielfts <d.trivino@uniandes.edu.co>
2024-05-25 13:34:28 -0600

    moved dummy to controller

--- src/controllers/MatchController.ts
+++ src/controllers/MatchController.ts
@@ -5,6 +5,8 @@ import JSONResponse from "../dtos/JSONResponse";
 import MatchService from "../services/MatchService";
 import { StatusCodes } from "http-status-codes";
 import GoalDTO from "../dtos/goalDTO";
+import { v4 as uuidv4 } from 'uuid';
+import MatchDetailDTO from "../dtos/matchDetailDTO";
 
 class MatchController {
   public static async addGoal(req: Request, res: Response, next: NextFunction) {
@@ -51,6 +53,121 @@ class MatchController {
     } catch (error) {
       next(error);
     }
+  }
+
+  public static getDummyMatchesByTournament(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ){
+    const dummy: MatchDetailDTO[] = [
+      {
+        id: uuidv4(),
+        dateTimeStart: new Date(),
+        dateTimeEnd: new Date(),
+        isPlaying: true,
+        teamA: {
+          id: uuidv4(),
+          name: "santa fe",
+          points: 20,
+          shieldImg: "null",
+          goals: [
+            {
+              id: uuidv4(),
+              name: "pepito",
+              lastName: "gomelin",
+              minute: 90,
+              playerNumber: 10,
+            },
+            {
+              id: uuidv4(),
+              name: "juanito",
+              lastName: "ñero",
+              minute: 80,
+              playerNumber: 11,
+            },
+          ],
+        },
+        teamB: {
+          id: uuidv4(),
+          points: 18,
+          name: "junior",
+          shieldImg: "null",
+          goals: [
+            {
+              id: uuidv4(),
+              name: "pepito",
+              lastName: "caremonda",
+              minute: 90,
+              playerNumber: 10,
+            },
+            {
+              id: uuidv4(),
+              name: "juanito",
+              lastName: "caremonda",
+              minute: 80,
+              playerNumber: 11,
+            },
+          ],
+        },
+      },
+      {
+        id: uuidv4(),
+        dateTimeStart: new Date(),
+        dateTimeEnd: new Date(),
+        isPlaying: true,
+        teamA: {
+          id: uuidv4(),
+          name: "santa fe",
+          points: 20,
+          shieldImg: "null",
+          goals: [
+            {
+              id: uuidv4(),
+              name: "pepito",
+              lastName: "gomelin",
+              minute: 90,
+              playerNumber: 10,
+            },
+            {
+              id: uuidv4(),
+              name: "juanito",
+              lastName: "ñero",
+              minute: 80,
+              playerNumber: 11,
+            },
+          ],
+        },
+        teamB: {
+          id: uuidv4(),
+          points: 18,
+          name: "junior",
+          shieldImg: "null",
+          goals: [
+            {
+              id: uuidv4(),
+              name: "pepito",
+              lastName: "caremonda",
+              minute: 90,
+              playerNumber: 10,
+            },
+            {
+              id: uuidv4(),
+              name: "juanito",
+              lastName: "caremonda",
+              minute: 80,
+              playerNumber: 11,
+            },
+          ],
+        },
+      },
+    ];
+    const response: JSONResponse = {
+      success: true,
+      message: "Matches retrieved successfully",
+      data: dummy,
+    };
+    res.status(StatusCodes.OK).json(response);
   }
   public static async createMatch(
     req: Request,
