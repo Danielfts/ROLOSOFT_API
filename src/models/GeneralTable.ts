9a8a2ebe07f1b6a28e894d74aaeb47352d0f6d6e
Danielfts <d.trivino@uniandes.edu.co>
2024-05-27 17:25:15 -0600

    Added missing properties to general table

--- src/models/GeneralTable.ts
+++ src/models/GeneralTable.ts
@@ -1,66 +1,102 @@
 import { sequelize } from "../config/db";
-import {Model, DataTypes, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey, NonAttribute} from "sequelize";
+import {
+  Model,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  ForeignKey,
+  NonAttribute,
+} from "sequelize";
 import { UUID } from "crypto";
 import Team from "./Team";
 import { number } from "joi";
 
-class GeneralTable extends Model<InferAttributes<GeneralTable>, InferCreationAttributes<GeneralTable>> {
-  declare id: CreationOptional<UUID>
-  declare teamId: ForeignKey<UUID>
-  declare position : number
-  declare defeats: number
-  declare draws: number
-  declare victories: number
+class GeneralTable extends Model<
+  InferAttributes<GeneralTable>,
+  InferCreationAttributes<GeneralTable>
+> {
+  declare id: CreationOptional<UUID>;
+  declare teamId: ForeignKey<UUID>;
+  declare position: number;
+  declare defeats: number;
+  declare draws: number;
+  declare victories: number;
+  declare points: number;
+  declare goalsFor: number;
+  declare goalsAgainst: number;
+  declare goalDifference: number;
+  declare gamesPlayed: number;
 
   //Timestamps
   declare createdAt: CreationOptional<Date>;
   declare updatedAt: CreationOptional<Date>;
   declare deletedAt: CreationOptional<Date | null>;
   declare Team: NonAttribute<Team>;
-
 }
 
-GeneralTable.init({
-  id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-  },
-  teamId: {
-    type: DataTypes.UUID,
-    references: {
-      model: Team,
-      key: "id",
+GeneralTable.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
     },
-    unique: true,
+    teamId: {
+      type: DataTypes.UUID,
+      references: {
+        model: Team,
+        key: "id",
+      },
+      unique: true,
+    },
+    defeats: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    draws: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    victories: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    position: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    points: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    goalsFor: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    goalsAgainst: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    goalDifference: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    gamesPlayed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
   },
-  defeats: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-  draws: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-  victories: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-  },
-  deletedAt: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-  position: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-},
   {
     sequelize,
     tableName: "GeneralTable",
@@ -73,15 +109,15 @@ GeneralTable.init({
 GeneralTable.belongsTo(Team, {
   foreignKey: {
     name: "teamId",
-    allowNull: false
-  }
-})
+    allowNull: false,
+  },
+});
 
 Team.hasOne(GeneralTable, {
   foreignKey: {
     name: "teamId",
-    allowNull: false
-  }
-})
+    allowNull: false,
+  },
+});
 
 export default GeneralTable;
